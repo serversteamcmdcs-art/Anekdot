46f02895d6779e210e6735ce0972f2c00f06a3bb
serversteamcmdcs-art <serversteamcmdcs@gmail.com>
2026-03-01 19:18:31 +0500

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+app.use(express.static(path.join(__dirname, '/')));
+
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
