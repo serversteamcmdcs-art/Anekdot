700080b9235b015a2e5d8d4ff4263747d53ef82a
serversteamcmdcs-art <serversteamcmdcs@gmail.com>
2026-03-01 18:21:17 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Internal Server Error\n');
+            return;
+        }
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);
+    });
+});
+server.listen(3000, () => {
+    console.log(`Server running at http://127.0.0.1:3000/`);
+});
